f5973548e9f79317f2cedaa20bce158fe51958c0
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2008-03-14 13:35:08 +0000

    register utils and utility

--- slc/alertservice/__init__.py
+++ slc/alertservice/__init__.py
@@ -2,8 +2,13 @@
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
     
+    from AccessControl import allow_module
+    allow_module('slc.alertservice.utils')
+    
     import AlertserviceTool
     tools = (AlertserviceTool.AlertserviceTool, )
+    
+
     
     from Products.CMFPlone.utils import ToolInit
     # Register tools and content
@@ -11,3 +16,7 @@ def initialize(context):
              , tools=tools
              , icon='skins/alertservice/alertservice_icon.gif'
              ).initialize( context )
+
+
+from zope.i18nmessageid import MessageFactory
+AlertMessageFactory = MessageFactory('slc.alertservice')
