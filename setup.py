99404d7b2efc84c7fed47d661f5cbf6ee38ef131
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-05 14:10:31 +0000

    prepared egg releasey

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+This module contains the slc.alertservice package
+"""
 import os
+from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('slc', 'alertservice', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']
 
 setup(name='slc.alertservice',
       version=version,
       description="Alert subscription tool",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -17,7 +45,7 @@ setup(name='slc.alertservice',
       keywords='',
       author='syslab.com',
       author_email='thomas@syslab.com',
-      url='http://www.syslab.com',
+      url='https://svn.syslab.com/svn/syslabcom/slc.alertservice/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['slc'],
@@ -25,9 +53,15 @@ setup(name='slc.alertservice',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Products.AdvancedQuery',
           # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'slc.alertservice.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
